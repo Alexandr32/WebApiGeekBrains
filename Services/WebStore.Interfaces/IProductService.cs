9db150bfe70498a1dc7fcccc0ba252f015bbac2d
Alex <alex3289@mail.ru>
2020-10-25 14:17:36 +0500

    Код

---
+++ Services/WebStore.Interfaces/IProductService.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using WebStore.DomainNew.Dto;
+using WebStore.DomainNew.Entities;
+using WebStore.DomainNew.Filters;
+
+namespace WebStore.Interfaces
+{
+    public interface IProductService
+    {
+        IEnumerable<Section> GetSections();
+        IEnumerable<Brand> GetBrands();
+        PagedProductDto GetProducts(ProductFilter filter);
+
+        /// <summary>
+        /// Получить товар по Id
+        /// </summary>
+        /// <param name="id">Идентификатор</param>
+        /// <returns>Сущность Product, если нашел, иначе null</returns>
+        ProductDto GetProductById(int id);
+
+        /// <summary>Секция по Id</summary>
+        /// <param name="id">Id</param>
+        Section GetSectionById(int id);
+
+        /// <summary>Бренд по Id</summary>
+        /// <param name="id">Id</param>
+        Brand GetBrandById(int id);
+
+        /// <summary>Создать продукт</summary>
+        /// <param name="product">Сущность Product</param>
+        SaveResultDto CreateProduct(ProductDto product);
+
+        /// <summary>Обновить продукт</summary>
+        /// <param name="product">Сущность Product</param>
+        SaveResultDto UpdateProduct(ProductDto product);
+
+        /// <summary>Удалить продукт</summary>
+        /// <param name="productId">Id продукта</param>
+        SaveResultDto DeleteProduct(int productId);
+    }
+}
---
+++ Ui/WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebStore.DomainNew.Entities;
+using WebStore.DomainNew.Filters;
+using WebStore.DomainNew.Helper;
+using WebStore.DomainNew.ViewModels;
+using WebStore.Interfaces;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class HomeController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductList()
+        {
+            var products = _productService.GetProducts(new ProductFilter());
+            return View(products);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var notParentSections = _productService
+                .GetSections()
+                .Where(s => s.ParentId != null);
+            var brands = _productService.GetBrands();
+
+            // выполним проверки...
+            if (id is null)
+                return View(new ProductViewModel
+                {
+                    Sections = new SelectList(notParentSections, "Id", "Name"),
+                    Brands = new SelectList(brands, "Id", "Name")
+                });
+
+            var product = _productService.GetProductById(id.Value);
+            if (product is null)
+                return NotFound();
+
+            return View(product.ToViewModel(notParentSections, brands));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ProductViewModel model)
+        {
+            var notParentSections = _productService
+                .GetSections()
+                .Where(s => s.ParentId != null);
+            var brands = _productService.GetBrands();
+
+            if (ModelState.IsValid)
+            {
+                if (model.Id >0)
+                {
+                    _productService.UpdateProduct(model.ToDto());
+                }
+                else
+                {
+                    _productService.CreateProduct(model.ToDto());
+                }
+
+                return RedirectToAction(nameof(ProductList));
+            }
+
+            model.Brands = new SelectList(brands, "Id", "Name", model.BrandId);
+            model.Sections = new SelectList(notParentSections, "Id", "Name", model.SectionId);
+
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _productService.GetProductById(id);
+            if (product == null)
+                return NotFound();
+
+            return View(product.ToViewModel(new List<Section>(), new List<Brand>()));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(ProductViewModel model)
+        {
+            _productService.DeleteProduct(model.Id);
+            return RedirectToAction(nameof(ProductList));
+        }
+
+    }
+}
